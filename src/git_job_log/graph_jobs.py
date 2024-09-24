8e66d39e9d772f0b06ffec645bd738f50617551c
Terry Brown <Brown.TerryN@epa.gov>
2024-09-24 22:26:05 +0000

    Add status, key, etc.

--- src/git_job_log/graph_jobs.py
+++ src/git_job_log/graph_jobs.py
@@ -1,6 +1,11 @@
 """Plot job dependencies and status."""
+import time
 
 import pygraphviz as pgv
+from git_job_log import LastRun
+
+FILL_GOOD = "#88aaff"
+FILL_BAD = "orange"
 
 
 def make_graph(depends):
@@ -8,11 +13,35 @@ def make_graph(depends):
     graph = pgv.AGraph()
     for edge in depends:
         graph.add_edge(edge)
+    graph.graph_attr["label"] = time.asctime()
+    graph.edge_attr["dir"] = "forward"
+    # graph.node_attr["fontname"] = "sans-serif"
     return graph
 
 
+def add_key(graph):
+    """Add a key, use after tests are run."""
+    key = graph.add_subgraph(None, "cluster_key")
+    key.graph_attr["label"] = "Key"
+    # key.graph_attr["ranksep"] = 0.5
+    key_nodes = ("current_", "stale_")
+    key.add_edge(key_nodes)
+    edge = key.get_edge(*key_nodes)
+    edge.attr["style"] = "invis"
+    for node_id in key_nodes:
+        node = key.get_node(node_id)
+        node.attr["shape"] = "box"
+        node.attr["width"] = 1
+        node.attr["label"] = node_id.strip("_").title()
+        node.attr["style"] = "filled"
+        node.attr["fillcolor"] = {"current_": FILL_GOOD, "stale_": FILL_BAD}[node_id]
+
+
 def label(node_id: str, max_len: int = 16) -> str:
-    """Node label from node_id."""
+    """Node label from node_id.
+
+    Returns labels no more than max_len+1 long, +1 for trailing /."
+    """
     texts = [[]]
     steps = node_id.split("/")
     for step_i, step in enumerate(steps):
@@ -25,9 +54,48 @@ def label(node_id: str, max_len: int = 16) -> str:
     return "\\n".join("".join(i) for i in texts)
 
 
-def make_plot(graph, out_path):
+def make_plot(graph, out_path, with_key=True):
     """Make SVG plot of graph."""
     for node_id in graph:
         node = graph.get_node(node_id)
         node.attr["label"] = label(node_id)
+        node.attr["tooltip"] = f"{node_id}\\nlast_ran: {node.attr['run_at']}"
+    if with_key:
+        add_key(graph)
     graph.draw(out_path, prog="dot")
+
+
+def recurse_status(graph, head, job_ran, ok):
+    """Recursively set status of jobs."""
+    graph.get_node(head).attr["style"] = "filled"
+    graph.get_node(head).attr["run_at"] = (
+        job_ran[head].timestamp if job_ran[head].timestamp else "NEVER"
+    )
+    if not ok:
+        graph.get_node(head).attr["fillcolor"] = FILL_BAD
+        for child in graph.out_neighbors(head):
+            recurse_status(graph, child, job_ran, ok)
+    for child in graph.out_neighbors(head):
+        ok = (
+            job_ran[head].timestamp is not None
+            and job_ran[child].timestamp is not None
+            and job_ran[child].timestamp >= job_ran[head].timestamp
+        )
+        recurse_status(graph, child, job_ran, ok)
+
+
+def add_status(graph, gjl):
+    """Add up to dateness."""
+    job_ran = gjl.last_runs()
+    for job in graph:
+        if job not in job_ran:
+            job_ran[job] = LastRun(timestamp=None, data=None)
+
+    for node_id in graph:
+        node = graph.get_node(node_id)
+        node.attr["fillcolor"] = FILL_GOOD
+        node.attr["style"] = "filled"
+    in_degree = dict(zip(graph, graph.in_degree()))
+    heads = [i for i in graph if in_degree[i] == 0]
+    for head in heads:
+        recurse_status(graph, head, job_ran, ok=job_ran[head].timestamp is not None)
--- tests/test_graph_jobs.py
+++ tests/test_graph_jobs.py
@@ -1,8 +1,10 @@
 """Tests for graph_jobs."""
 
+import time
 from itertools import chain
 
-from git_job_log import GitJobLog, LastRun, graph_jobs
+from git_job_log import GitJobLog, graph_jobs
+from git_job_log.graph_jobs import FILL_BAD, FILL_GOOD
 
 DEPENDS = [
     ("home/yard/season/spring", "home/yard/lawn/get_gas"),
@@ -10,6 +12,8 @@ DEPENDS = [
     ("home/yard/lawn/mow", "home/yard/lawn/compost_clippings"),
     ("home/yard/lawn/get_gas", "home/yard/tools/find/gas_tank"),
     ("home/yard/tools/find/gas_tank", "home/yard/shed/organize"),
+    # Separate tree
+    ("work/commute/pass/expired", "work/commute/pass/renew"),
 ]
 
 VERTICES = list(set(chain.from_iterable(DEPENDS)))
@@ -45,16 +49,82 @@ def test_labels():
 
 def test_graph_jobs(random_remote):
     """Test we can just graph the jobs"""
-    gjl = GitJobLog(random_remote)
-    job_ran = gjl.last_runs()
-    for job in VERTICES:
-        if job not in job_ran:
-            job_ran[job] = LastRun(timestamp=None, data=None)
     graph = graph_jobs.make_graph(DEPENDS)
     out_path = random_remote / "test.svg"
-    graph_jobs.make_plot(graph, out_path)
-    graph_jobs.make_plot(graph, "test2.svg")
+    graph_jobs.make_plot(graph, out_path, with_key=False)
     assert out_path.exists()
     text = out_path.read_text()
     print(VERTICES)
+    # Note the full text (node_id) occurs in the <title/> element, not the label.
     assert all([i in text for i in VERTICES])
+
+    graph_jobs.make_plot(graph, "test2.svg")
+
+
+def test_graph_deps_all_done(random_remote):
+    """Test with all jobs run at same time."""
+    gjl = GitJobLog(random_remote)
+    gjl.log_run(VERTICES)
+    graph = graph_jobs.make_graph(DEPENDS)
+    graph_jobs.add_status(graph, gjl)
+    out_path = random_remote / "test.svg"
+    graph_jobs.make_plot(graph, out_path, with_key=False)
+    assert out_path.exists()
+    text = out_path.read_text()
+    assert text.count("NEVER") == 0
+    assert text.count(FILL_BAD) == 0
+    assert text.count(FILL_GOOD) == len(VERTICES)
+
+    graph_jobs.make_plot(graph, "test3.svg")
+
+
+def test_graph_deps_none_done(random_remote):
+    """Test with all jobs run at same time."""
+    gjl = GitJobLog(random_remote)
+    graph = graph_jobs.make_graph(DEPENDS)
+    graph_jobs.add_status(graph, gjl)
+    out_path = random_remote / "test.svg"
+    graph_jobs.make_plot(graph, out_path, with_key=False)
+    assert out_path.exists()
+    text = out_path.read_text()
+    assert text.count("NEVER") == len(VERTICES)
+    assert text.count(FILL_BAD) == len(VERTICES)
+    assert text.count(FILL_GOOD) == 0
+
+    graph_jobs.make_plot(graph, "test4.svg")
+
+
+def test_graph_deps_some_done(random_remote):
+    """Test with a job never run."""
+    gjl = GitJobLog(random_remote)
+    gjl.log_run(i for i in VERTICES if i != "home/yard/lawn/get_gas")
+    graph = graph_jobs.make_graph(DEPENDS)
+    graph_jobs.add_status(graph, gjl)
+    out_path = random_remote / "test.svg"
+    graph_jobs.make_plot(graph, out_path, with_key=False)
+    assert out_path.exists()
+    text = out_path.read_text()
+    assert text.count("NEVER") == 1  # "home/yard/lawn/get_gas"
+    assert text.count(FILL_BAD) == 5  # above plus its four descendants
+    assert text.count(FILL_GOOD) == len(VERTICES) - 5
+
+    graph_jobs.make_plot(graph, "test5.svg")
+
+
+def test_graph_deps_updated(random_remote):
+    """Test with a job run later."""
+    gjl = GitJobLog(random_remote)
+    gjl.log_run(VERTICES)
+    time.sleep(2)
+    gjl.log_run(["home/yard/lawn/get_gas"])
+    graph = graph_jobs.make_graph(DEPENDS)
+    graph_jobs.add_status(graph, gjl)
+    out_path = random_remote / "test.svg"
+    graph_jobs.make_plot(graph, out_path, with_key=False)
+    assert out_path.exists()
+    text = out_path.read_text()
+    assert text.count("NEVER") == 0
+    assert text.count(FILL_BAD) == 4  # "home/yard/lawn/get_gas"'s four descendants
+    assert text.count(FILL_GOOD) == len(VERTICES) - 4
+
+    graph_jobs.make_plot(graph, "test6.svg")
